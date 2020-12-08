21af4727b0d7e2db6fbe50256b2b773b6607c405
Jonathan Huang <jonnohuang@hotmail.com>
2020-12-08 09:49:24 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PickDay from "./Components/PickDay";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Happiibook Intervew</h1>
+      <h2>Calender Day Picker</h2>
+      <PickDay />
+    </div>
+  );
+}
